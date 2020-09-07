e49866fff0b5cdae163d0545e84506c2781c276a
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-09-07 18:12:54 -0500

    built navbar component,fixed logo positioning,and added searchbox using material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import Header from './components/header/header.component';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 function App() {
   return (
     <Router>
       <div className="App">
       <Switch>
+        {/* Switch to Checkout Page */}
         <Route path = "/checkout">
           <h1>Checkout</h1>
         </Route>
+        {/* Switch to Login Page */}
         <Route path="/login">
           <h1>Login Page</h1>
         </Route>
+        {/* Default Page */}
         <Route path="/">
+          <Header />
           <HomePage />
         </Route>
       </Switch>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import './header.styles.scss';
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+
 const Header = () => (
-    <div>
-        <h1> Navigation Bar </h1>
-    </div>
+
+    <nav className="header">
+        {/* Logo for the Navigation Bar */}
+        <Link to="/">
+            <img className="header__logo" src="logo.png" alt="" />
+        </Link>
+
+        {/* Search Box */}
+        <div className="header__search">
+            <input type="text" className="header__searchInput" />
+            <SearchIcon className="header__searchIcon" />
+        </div>
+
+        {/* Other Links */}
+        {/* Cart Icon with Number */}
+
+    </nav>
+
 );
 
 export default Header;
