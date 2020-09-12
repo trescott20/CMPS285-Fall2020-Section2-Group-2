bd285ace29dcb1a9cea5626711b1007686538f0d
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-09-12 09:27:39 -0500

    update menu-items to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -3,26 +3,44 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import Header from './components/header/header.component';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import CheckOut from '../src/pages/checkout/checkout.component'
+
 function App() {
+
+  const MobileDevices = () => (
+    <div>
+      <h1>Mobile Devices</h1>
+    </div>
+  );
+
   return (
     <Router>
       <div className="App">
       <Switch>
+
         {/* Switch to Checkout Page */}
         <Route path = "/checkout">
           <Header />
-          <h1>Checkout</h1>
+          <CheckOut/>
         </Route>
+
         {/* Switch to Login Page */}
         <Route path="/login">
           <Header />
           <h1>Login Page</h1>
         </Route>
+
         {/* Default Page */}
-        <Route path="/">
+        <Route exact path="/" >
           <Header />
           <HomePage />
         </Route>
+
+        {/*--------------- Directory Pages ------------- */}
+
+        {/* Mob-Devices Page */}
+        <Route path='/mobiledevices' component={MobileDevices} />
+
       </Switch>
     </div>
     </Router>
