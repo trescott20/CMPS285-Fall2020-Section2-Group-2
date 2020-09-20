a83aba6eb1b41023fa0ff899028d63c38d32752c
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-09-19 19:38:26 -0500

    modified the alignment of sign-in and sign-out form for login page

--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -4,9 +4,11 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-out'>
+    <div className='login'>
+        <div className='sign-in-and-sign-out'>
         <SignIn/>
         <SignUp/>
+    </div>
     </div>
 );
 
